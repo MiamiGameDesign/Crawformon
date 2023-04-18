8493dad92a65e0109016e939562d5319727300e8
MiamiGameDesign <gamedesignclub@miamioh.edu>
2023-04-18 19:09:31 -0400

    Push

--- Assets/AllTextOnScreen.cs
+++ Assets/AllTextOnScreen.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 
 public class AllTextOnScreen : MonoBehaviour
 {
+    public GameObject playerHealth;
     public GameObject whatWillYouDo;
     public GameObject bossHealth;
     public Image youWinB;
     public GameObject youwinT;
     static public double currHealth = 100;
+    static public double currPHealth = 100;
     static public double maxHealth = 100;
     
     // Start is called before the first frame update
@@ -23,10 +25,16 @@ public class AllTextOnScreen : MonoBehaviour
     void Update()
     {
         bossHealth.GetComponent<Text>().text = "Crawford's Health: " + currHealth + "/" + maxHealth;
+        playerHealth.GetComponent<Text>().text = "Player's Health: " + currPHealth + "/" + maxHealth;
         if (currHealth <=0)
         {
             youWinB.enabled = true;
             youwinT.GetComponent<Text>().text = "YOU WIN!!!! :D";
+        }
+        if (currPHealth <=0)
+        {
+            youWinB.enabled = true;
+            youwinT.GetComponent<Text>().text = "YOU Lose!!!! D:";
         }
     }
 }
--- Assets/Attack.cs
+++ Assets/Attack.cs
@@ -14,12 +14,22 @@ public class Attack : MonoBehaviour
         "Kraw Daddy Used Suspend",
         "Kraw Daddy Used Expel"
     };
+
+    public AudioSource sound;
+
     //give player a certain amount of seconds to push a key
     //make battle start be greg crawford just saying his name really slowly
     public void OnPress()
     {
+        sound.Play();
         GameObject.FindWithTag("KrawDaddy").GetComponent<Text>().text = krawdaddyText[Random.Range(0, krawdaddyText.Length)];
         if (AllTextOnScreen.currHealth > 0)
         AllTextOnScreen.currHealth -= (int)Random.Range(0, 10);
+
+        if (AllTextOnScreen.currPHealth > 0)
+        AllTextOnScreen.currPHealth -= (int)Random.Range(0, 10);
     }
+
+    
+
 }
